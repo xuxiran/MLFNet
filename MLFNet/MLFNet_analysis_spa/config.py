00b69cb7e324160bf5c4d16101a5ccf67464f238
Xiran Xu <48015859+xuxiran@users.noreply.github.com>
2025-01-24 16:22:29 +0800

    Update config.py

--- MLFNet/MLFNet_analysis_spa/config.py
+++ MLFNet/MLFNet_analysis_spa/config.py
@@ -1,7 +1,7 @@
 import torch
 # OURmodels
  # you could change the code to other OURmodels by only changing the number
-process_data_dir = '../../precessed_data/'
+process_data_dir = '../../preprocess_data/'
 
 lr = 1e-3
 weight_decay = 0.01
